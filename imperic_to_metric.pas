3ed7c71ddbaf3227f692f6305157fd13f5ea5f8b
GrispenX <grispenx@gmail.com>
2023-10-07 01:25:47 +0300

    Add imperic_to_metric.pas

---
+++ imperic_to_metric.pas
@@ -0,0 +1,12 @@
+﻿program apb;
+
+var
+  fut, duym:integer;
+  meter:real;
+
+begin
+  readLn(fut);
+  readLn(duym);
+  meter := ((fut * 12 + duym) * 25.4) / 1000;
+  writeLn(fut,'''', duym, '" = ', meter:0:2, 'm.');
+end.  
